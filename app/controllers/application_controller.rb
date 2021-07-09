109a3c2119baa6282dcfbe8e536ab48cd367508b
tmampa <tmampa94@gmail.com>
2021-07-09 13:50:53 +0200

    Fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+  def require_login
+    unless current_user
+      redirect_to login_path, :alert => 'Sign up or Log in to see the events!'
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
 end
