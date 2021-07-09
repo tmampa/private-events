ed68a135bd742d8a7e5439ab6cc158b2db2b802e
tmampa <tmampa94@gmail.com>
2021-07-09 14:16:18 +0200

    Migrate databases

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# rubocop:disable Metrics/BlockLength
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,40 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_610_101_247) do
-  create_table 'attended_events', force: :cascade do |t|
-    t.integer 'event_id', null: false
-    t.integer 'user_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['event_id'], name: 'index_attended_events_on_event_id'
-    t.index ['user_id'], name: 'index_attended_events_on_user_id'
+ActiveRecord::Schema.define(version: 2021_06_10_101247) do
+
+  create_table "attended_events", force: :cascade do |t|
+    t.integer "event_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_attended_events_on_event_id"
+    t.index ["user_id"], name: "index_attended_events_on_user_id"
   end
 
-  create_table 'events', force: :cascade do |t|
-    t.string 'name'
-    t.date 'date'
-    t.string 'location'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.integer 'user_id', null: false
-    t.index ['user_id'], name: 'index_events_on_user_id'
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.date "date"
+    t.string "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'attended_events', 'events'
-  add_foreign_key 'attended_events', 'users'
-  add_foreign_key 'events', 'users'
+  add_foreign_key "attended_events", "events"
+  add_foreign_key "attended_events", "users"
+  add_foreign_key "events", "users"
 end
-# rubocop:enable Metrics/BlockLength
