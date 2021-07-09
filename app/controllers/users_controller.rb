fb90e372fdb2706c7b19ad130d7130058fea900f
tmampa <tmampa94@gmail.com>
2021-07-09 13:49:59 +0200

    Event attendance

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  include UsersHelper
+  before_action :require_login, except: [:new, :create]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    session[:user_id] = @user.id
+    flash.notice = "User #{@user.name} successfully created! Thank you for signing up!"
+    redirect_to user_path(@user)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+end
