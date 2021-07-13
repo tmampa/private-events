3176c11db02d1e39e0405dd1bae8ac71463b21d4
tmampa <tmampa94@gmail.com>
2021-07-13 16:36:27 +0200

    Show attened users

--- app/controllers/attended_events_controller.rb
+++ app/controllers/attended_events_controller.rb
@@ -1,24 +1,13 @@
-class AttendedEventsController < ApplicationController
-  def show
-    event = Event.find(params[:event_id])
-    @attend = AttendedEvent.where('event_id = ?', event.id)
-  end
-
+class AttendedEventController < ApplicationController
   def create
     event = Event.find(params[:event_id])
-    unless user_signed_in?
-      redirect_to new_user_session_path
-      return
-    end
-    @attended_event = AttendedEvent.new(event_id: event.id, user_id: current_user.id)
-    if AttendedEvent.where(user_id: current_user.id, event_id: params[:event_id]).exists?
-      redirect_to event_path(event)
-      return
-    end
-    if event.user_id == current_user.id
-      redirect_to event_path(event)
-      return
-    end
-    redirect_back(fallback_location: root_path) if @attended_event.save
+    attended = event.attended_event.find_by(attendee_id: current_user.id)
+    redirect_to event_path(event) and return if attended
+
+    @attended_event = event.attended_event.build(attendee_id: current_user.id)
+    redirect_to event_path(event) and return if @attended_event.save
+
+    redirect_to event_path(event)
   end
 end
+
